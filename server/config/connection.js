43c927c1fe0e62e0761be6f0ff09e4e2e473884d
Charanvir <charanvir123@gmail.com>
2022-10-26 03:07:49 -0400

    saved mongoDB credentials to .env

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://charanvir123:Capsfan123@cluster0.r1vb1cu.mongodb.net/F1_Fantasy?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.r1vb1cu.mongodb.net/F1_Fantasy?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
